208f6afe61edcce9ae7e31ebc57f9eec04b25839
Miguel Muscat <miguelmuscat93@gmail.com>
2017-01-19 20:47:56 +0100

    `AppInterface` extends `ComponentAwareInterface`

--- src/AppInterface.php
+++ src/AppInterface.php
@@ -7,40 +7,6 @@ namespace Dhii\App;
  *
  * @since [*next-version*]
  */
-interface AppInterface
+interface AppInterface extends ComponentAwareInterface
 {
-    /**
-     * Retrieve an application component by code.
-     *
-     * @since [*next-version*]
-     *
-     * @return ComponentInterface
-     */
-    public function getComponent($code);
-
-    /**
-     * Register a component with this instnace.
-     *
-     * @since [*next-version*]
-     *
-     * @param ComponentInterface $component The component to register.
-     * @param string             $code      The code to register the component with.
-     *
-     * @throws RuntimeException If a component with the specified code is already registered.
-     *
-     * @return WpInterface This instance.
-     */
-    public function registerComponent(ComponentInterface $component, $code);
-
-    /**
-     * Determines whether or not a component with the specified code is already registered with this instance.
-     *
-     * @since [*next-version*]
-     *
-     * @param string $code The code of the component.
-     *
-     * @return bool True if a component with the specified code is already registered;
-     *              false otherwise.
-     */
-    public function hasComponent($code);
 }
